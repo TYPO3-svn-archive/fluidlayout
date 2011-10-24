e844093acfc9d9db0792f4151a45db0ebcf571c0
sorenmalling <sorenmalling@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-24 06:10:16 +0000

    Updated emconf before release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fluidlayout/trunk@53291 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "fluidlayout".
 #
-# Auto generated 27-09-2011 14:05
+# Auto generated 24-10-2011 08:08
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"87af";s:10:"README.txt";s:4:"ee2d";s:32:"class.tx_fluidlayout_tcemain.php";s:4:"5376";s:16:"ext_autoload.php";s:4:"e3a2";s:12:"ext_icon.gif";s:4:"8db7";s:17:"ext_localconf.php";s:4:"90a8";s:14:"ext_tables.php";s:4:"1665";s:14:"ext_tables.sql";s:4:"a47b";s:32:"icon_tx_fluidlayout_template.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"0edc";s:7:"tca.php";s:4:"39c6";s:14:"doc/manual.sxw";s:4:"5223";s:19:"doc/wizard_form.dat";s:4:"712e";s:20:"doc/wizard_form.html";s:4:"1009";s:16:"static/setup.txt";s:4:"884f";}',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"87af";s:10:"README.txt";s:4:"ee2d";s:32:"class.tx_fluidlayout_tcemain.php";s:4:"b022";s:16:"ext_autoload.php";s:4:"e3a2";s:12:"ext_icon.gif";s:4:"8db7";s:17:"ext_localconf.php";s:4:"90a8";s:14:"ext_tables.php";s:4:"fd55";s:14:"ext_tables.sql";s:4:"a47b";s:32:"icon_tx_fluidlayout_template.gif";s:4:"8db7";s:16:"locallang_db.xml";s:4:"0edc";s:7:"tca.php";s:4:"49b7";s:14:"doc/manual.sxw";s:4:"5223";s:19:"doc/wizard_form.dat";s:4:"712e";s:20:"doc/wizard_form.html";s:4:"1009";s:24:"static/content/setup.txt";s:4:"172f";s:25:"static/template/setup.txt";s:4:"7532";}',
 	'suggests' => array(
 	),
 );
